0c00f78287679a2ebe599a3f4f846a55e9cfa3c1
Alejandro Sanchez <alesanvm@gmail.com>
2022-08-31 21:37:32 -0300

    Contador

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,9 +1,21 @@
 
 import React from 'react'
+import ItemCount from './ItemCount/ItemCount'
 
-const ItemListContainer = () => {
+const ItemListContainer = ({greeting='Hola'}) => {
+  
+  const onAdd = (cantidad) => {
+    console.log(`Compraste ${cantidad} unidades`);
+  }
+
   return (
-    <div>Listado de Productos</div>
+    <>
+    <div>
+      <h2>{greeting}</h2>
+      <h4>Listado de productos</h4>
+      <ItemCount initial={1} stock={10} onAdd={onAdd} />
+    </div>
+    </>
   )
 }
 
